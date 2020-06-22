f8168376520194ab3c6b1ca739e91f61cf077722
Nemanja Tomanović <nemanja.tomanovic@gmail.com>
2020-06-22 06:49:02 +0200

    Added beer entity

--- src/test/java/com/nemanja/msbeerservice/MsBeerServiceApplicationTests.java
+++ src/test/java/com/nemanja/msbeerservice/MsBeerServiceApplicationTests.java
@@ -1,13 +1,75 @@
 package com.nemanja.msbeerservice;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nemanja.msbeerservice.web.model.BeerDto;
+import com.nemanja.msbeerservice.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class MsBeerServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void getBeerTest() throws Exception {
+
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void saveNewBeerTest() throws Exception {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Miss Quincy")
+                .beerStyle(BeerStyleEnum.ALE)
+                .build();
+
+        mockMvc.perform(post("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerDto)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void updateBeerTest() throws Exception {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Miss Quincy")
+                .beerStyle(BeerStyleEnum.ALE)
+                .build();
+
+        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerDto)))
+                .andExpect(status().isNoContent());
+    }
 
 }
