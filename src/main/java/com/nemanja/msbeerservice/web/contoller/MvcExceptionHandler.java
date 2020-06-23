ce5a99f68373f452d06fade66d6239cb5465d9c2
Nemanja Tomanović <nemanja.tomanovic@gmail.com>
2020-06-23 07:01:09 +0200

    Added controller advice for error handle on Beer controller

---
+++ src/main/java/com/nemanja/msbeerservice/web/contoller/MvcExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.nemanja.msbeerservice.web.contoller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+        List<String> errorList = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(error -> errorList.add("Error occurred " + error.toString()));
+
+        return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
+    }
+}
