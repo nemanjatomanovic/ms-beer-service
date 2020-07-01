37ad2cf0f8ddff1495ffa637aa62c66efc554fa3
Nemanja Tomanović <nemanja.tomanovic@gmail.com>
2020-07-01 10:04:40 +0200

    Added json format to fields of BeerDto

--- src/main/java/com/nemanja/msbeerservice/web/model/BeerDto.java
+++ src/main/java/com/nemanja/msbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.nemanja.msbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,9 +24,11 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime creationDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -35,6 +38,7 @@ public class BeerDto {
     @NotNull
     private BeerStyleEnum beerStyle;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private Long upc;
